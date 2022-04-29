ccd72f278c9afd1bf397f7c3d86d88ab51e2f204
Evan <53529192+BruceMcRooster@users.noreply.github.com>
2022-04-29 07:11:49 -0400

    Fixed problem with mix of spaces and tabs
    Godot was throwing errors due to a mix of spaces and tabs in the regular_polygon2d_node.gd file

--- addons/regular_polygon2d_node/regular_polygon2d_node.gd
+++ addons/regular_polygon2d_node/regular_polygon2d_node.gd
@@ -5,4 +5,4 @@ func _enter_tree():
     add_custom_type("RegularPolygon2D", "Node2D", preload("RegularPolygon2D.gd"), preload("addon_icon.png"))
 
 func _exit_tree():
-  	remove_custom_type("RegularPolygon2D")
+    remove_custom_type("RegularPolygon2D")
